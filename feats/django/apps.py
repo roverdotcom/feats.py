ecc0ed3e7ba47a2b8c18ad9f0dfce5a04f09bb48
Mike Chaten <mike.chaten@rover.com>
2019-12-30 14:38:07 -0800

    Let the Feats app be configured through django settings

--- feats/django/apps.py
+++ feats/django/apps.py
@@ -1,13 +1,29 @@
 from django.apps import AppConfig
+from django.core.exceptions import ImproperlyConfigured, AppRegistryNotReady
+from django.conf import settings
 from feats import App
-from feats.storage import Memory
 
-feats = App(storage=Memory())
 
 class FeatsConfig(AppConfig):
     name = 'feats.django'
     label = 'feats'
 
+    def ready(self):
+        self.feats_app = settings.get('FEATS')
+        if self.feats_app is None:
+            raise ImproperlyConfigured(
+                "The setting FEATS was not set. It must be set to a feats.App"
+            )
+        if not isinstance(self.feats_app, App):
+            raise ImproperlyConfigured(
+                "The setting FEATS must be a feats.App, was {}",
+                type(self.feats_app)
+            )
+
     @property
     def feats_app(self):
-        return feats
+        # Overwritten in ready(). Not allowed to use this until the app is
+        # configured
+        raise AppRegistryNotReady(
+            "Cannot use the Feats AppConfig until Django has been initialized"
+        )
--- feats/django/models.py
+++ feats/django/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class FeatsAccess(models.Model):
     """
-    Exists only for the content-type required for permissions
+    Fake Model, Exists only for the content-type required for permissions
     """
     class Meta:
         managed = False
