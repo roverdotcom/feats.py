ea77c8cab0688ae4f1dfc6d5282140f039ad3a84
Mike Chaten <mike.chaten@rover.com>
2020-01-02 14:19:29 -0800

    Split out add/change. Add urls/views. Change view not complete

--- feats/django/urls.py
+++ feats/django/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 app_name = 'feats'
 urlpatterns = [
-        url(r'^([^/]+)/edit/$', views.Change.as_view(), name='update'),
-        url(r'^([^/]+)/$', views.Detail.as_view(), name='detail'),
-        url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^([^/]+)/selectors/$', views.AddSelector.as_view(), name='add-selector')
+    url(r'^([^/]+)/selectors/([^/]+)/$', views.ChangeSelector.as_view(), name='update-selector'),
+    url(r'^([^/]+)/segmentation/$', views.ChangeSegmentation.as_view(), name='feature-segmentation'),
+    url(r'^([^/]+)/$', views.Detail.as_view(), name='detail'),
+    url(r'^$', views.Index.as_view(), name='index'),
 ]
--- feats/django/views.py
+++ feats/django/views.py
@@ -22,10 +22,46 @@ class Detail(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(self.feature)
         context['feature'] = self.feature
         return context
 
 
-class Change(TemplateView):
-    template_name = 'feats/change.html'
+class ChangeSegmentation(TemplateView):
+    template_name = 'feats/change_segmentation.html'
+
+    @property
+    def feature(self):
+        return app_config.feats_app.features[self.args[0]]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['feature'] = self.feature
+
+
+class AddSelector(TemplateView):
+    template_name = 'feats/add_selector.html'
+
+    @property
+    def feature(self):
+        return app_config.feats_app.features[self.args[0]]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['feature'] = self.feature
+
+
+class ChangeSelector(TemplateView):
+    template_name = 'feats/change_selector.html'
+
+    @property
+    def feature(self):
+        return app_config.feats_app.features[self.args[0]]
+
+    @property
+    def selector(self):
+        return self.feature.selectors[int(self.args[1])]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['feature'] = self.feature
+        context['selector'] = self.selector
