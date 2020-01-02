e0b09f314ee2d1dc5b7766160dcf2dd8880fdc6f
Mike Chaten <mike.chaten@rover.com>
2020-01-02 14:20:29 -0800

    Syntax

--- feats/django/urls.py
+++ feats/django/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'feats'
 urlpatterns = [
-    url(r'^([^/]+)/selectors/$', views.AddSelector.as_view(), name='add-selector')
+    url(r'^([^/]+)/selectors/$', views.AddSelector.as_view(), name='add-selector'),
     url(r'^([^/]+)/selectors/([^/]+)/$', views.ChangeSelector.as_view(), name='update-selector'),
     url(r'^([^/]+)/segmentation/$', views.ChangeSegmentation.as_view(), name='feature-segmentation'),
     url(r'^([^/]+)/$', views.Detail.as_view(), name='detail'),
