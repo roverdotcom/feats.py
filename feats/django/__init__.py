d1427b5a88385eb8ca412db5d84dcd28faa9b817
Mike Chaten <mike.chaten@rover.com>
2020-01-24 13:31:15 -0800

    Remove django for views PR

--- feats/django/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'feats.django.apps.FeatsConfig'
--- feats/django/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class FeatsAccess(models.Model):
-    """
-    Fake Model, Exists only for the content-type required for permissions
-    """
-    class Meta:
-        managed = False
